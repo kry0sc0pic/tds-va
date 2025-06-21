cf641e42d0792e43794e3994a90c007450a5834f
Krishaay <krishaayjois@gmail.com>
2025-06-21 05:46:42 +0000

    changes

--- main.py
+++ main.py
@@ -1,81 +1,51 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-import uvicorn
-import os
-import shutil
+import requests
+from bs4 import BeautifulSoup
 from fastapi import FastAPI
-from pydantic import BaseModel
-from langchain_chroma import Chroma
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAIEmbeddings 
+from fastapi.middleware.cors import CORSMiddleware
+# r = requests.get('https://weather-broker-cdn.api.bbci.co.uk/en/forecast/aggregated/264371').json()
 
-if 'chroma_langchain_db' not in os.listdir():
-    # os.system('unzip chroma_langchain_db.zip')
-    print("Unpackign Folder")
-    shutil.unpack_archive('./chroma_langchain_db.zip','./','zip')
+# wd = {}
 
-llm = ChatOpenAI()
-embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-vector_store = Chroma(
-    collection_name='example_collection',
-    embedding_function=embeddings,
-    persist_directory='./chroma_langchain_db'
-)
-retriever = vector_store.as_retriever()
+# for forecast in r['forecasts']:
+#     report = forecast['summary']['report']
+#     desc = report['enhancedWeatherDescription']
+#     local_date = report['localDate']
+#     wd[local_date] = desc
 
-system_prompt = (
-    "Use the given context to answer the question. "
-    "If you don't know the answer, say you don't know. "
-    "Use three sentence maximum and keep the answer concise. "
-    "Context: {context}"
-)
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-)
-question_answer_chain = create_stuff_documents_chain(llm, prompt)
-chain = create_retrieval_chain(retriever, question_answer_chain)
-
-class LinkData(BaseModel):
-    url: str
-    text: str
-
-class ResponseData(BaseModel):
-    answer: str
-    links: list[LinkData]
-
-
-
-class RequestData(BaseModel):
-    question: str
-    image: str | None
+# with open('f.txt','w') as f:
+#     f.write(str(wd))
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+r = requests.get('https://en.wikipedia.org/wiki/India')
+parsed = BeautifulSoup(r.content)
 
-@app.post('/api')
-async def api(body: RequestData):
-    q = body.question
-    result = chain.invoke({"input": q})
-    links = []
-    for c in result['context']:
-        link = {
-            "text": c.page_content,
-            "url": f'https://discourse.onlinedegree.iitm.ac.in'+c.metadata['url']
-        }
-        links.append(link)
-    res = {
-        "answer": result['answer'],
-        "links": links
-    }
-    return res
 
+
+
+
+# print(l)
+
+@app.get('/')
+async def index(country: str):
+    if len(country.strip()) == 0:
+        return "invalid", 500
+    url = f'https://en.wikipedia.org/wiki/{country.capitalize()}'
+    r = requests.get(url)
+    parsed = BeautifulSoup(r.content)
+    p = parsed.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+    final = ""
+    for element in p:
+        level = int(element.name.replace('h',''))
+        final+="#"*level + " " + element.text + "\n\n"
+    return final.strip(), 200
 
 if __name__ == '__main__':
-    uvicorn.run(app,host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
+    import uvicorn
+    uvicorn.run(app,host='0.0.0.0',port=8000)
